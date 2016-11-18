920e2a671cd805695f6e06a6ee3cd5a3a940babe
weaming <garden.yuen@gmail.com>
2016-11-18 23:14:52 +0800

    polipo

--- shadowsocks/install-polipo.sh
+++ shadowsocks/install-polipo.sh
@@ -4,8 +4,12 @@
 # 运行成功后，设置ｂａｓｈ环境变量 http_proxy 为 localhost:8123
 
 sudo apt-get install polipo &&
-    sudo sh -c 'cat ./polipo.config >> /etc/polipo/config' &&
-    sudo service polipo restart && echo 'Restart service success!'
+    sudo service polipo restart 2>/dev/null && echo 'Restart service success!'
+
+sudo cp ./polipo.config /etc/polipo/config &&
+sudo cp ./start-polipo.sh /usr/local/bin/start-polipo &&
+sudo chmod a+x /usr/local/bin/start-polipo
 
 echo '运行成功后，设置ｂａｓｈ环境变量 http_proxy 为 localhost:8123'
 echo '然后运行 curl ip.gs 进行测试'
+echo '运行 start-polipo 来启动/重启 polipo'

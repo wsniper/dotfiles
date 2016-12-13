c8c2f58c2c38b98faf25796c4100566e416d96ac
weaming <garden.yuen@gmail.com>
2016-12-14 01:17:29 +0800

    install brew and config mirrow

---
+++ mac/install-homebrew.sh
@@ -0,0 +1,40 @@
+#########################################################################
+# File Name: install-homebrew.sh
+# Author: weaming
+# mail: garden.yuen@gmail.com
+# Created Time: 2016-12-14 00:51:05
+#########################################################################
+#!/bin/bash
+
+if ! which git &>/dev/null ; then
+    echo 'Please install [git] first!'
+    exit 1
+fi
+
+if ! `which brew &>/dev/null` ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+fi
+
+# homebrew
+cd "$(brew --repo)" && git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
+
+# homebrew-core
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core" && git remote set-url origin git://mirrors.ustc.edu.cn/homebrew-core.git
+
+# homebrew-bottles
+ENV_STR='export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles'
+#if [ `echo $SHELL | grep -q zsh` ]; then
+#echo $ENV_STR >> ~/.zshrc
+#source ~/.zshrc
+#else
+#echo $ENV_STR >> ~/.bash_profile
+#source ~/.bash_profile
+#fi
+echo "================================================="
+echo "Please add \"$ENV_STR\" to your shell start file:"
+echo "~/.zshrc or ~/.bash_profile or other if you know what you are doing :)"
+echo "================================================="
+

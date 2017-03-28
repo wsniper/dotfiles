790f7072730f5e1233d20cdf4c7ce6bf23d4a2b1
weaming <garden.yuen@gmail.com>
2017-03-28 22:08:07 +0800

    minify shell code; daemonize polipo;

--- shadowsocks/fuckgfw.sh
+++ shadowsocks/fuckgfw.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$*" == "uninstall" ]; then
-    sudo pip uninstall shadowsocks
-    sudo rm -f /usr/local/bin/fuckgfw
+	sudo pip uninstall shadowsocks
+	sudo rm -f /usr/local/bin/fuckgfw
 else
-    sudo sslocal -c /etc/shadowsocks.json -d start
+	pid="$(sudo pgrep sslocal)"
+	[ "$pid" ] && sudo pkill sslocal && echo "killed $pid"
+	sudo sslocal -c /etc/shadowsocks.json -d start &&
+		echo "started with PID $(sudo pgrep sslocal)"
 fi
--- shadowsocks/start-polipo.sh
+++ shadowsocks/start-polipo.sh
@@ -1,29 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 # start polipo in background process. If it's running, will be killed and restart new.
 
 function start(){
-    dir=/var/log/polipo
-    log=$dir/error.log
-    [ ! -d $dir ] && sudo mkdir -p $dir
-    [ ! -w $dir ] && sudo chmod a+rwx $dir
-    nohup polipo -c /etc/polipo/config >/dev/null 2>$log &
+	sudo polipo -c /etc/polipo/config
 }
 
-function running(){
-    cat $log
+function restart(){
+	pid=$( pgrep -u root polipo )
+	[ "$pid" ] && sudo kill "$pid" 2>/dev/null && echo "Process $pid has been killed."
 
-    pid=$(ps aux | grep polipo | awk '$11=="polipo"{print $2}')
-    sudo kill $pid 2>/dev/null &&
-    echo "Process $pid has been killed."
-
-    rm -f $log
-    start
-    sleep 1
-    newpid=$(ps aux | grep polipo | awk '$11=="polipo"{print $2}')
-    echo "Restarted with PID $newpid"
+	start && sleep 1
+	newpid=$( pgrep -u root polipo )
+	echo "Restarted with PID $newpid"
 }
 
-start
-sleep 1
-[ -f $log ] && running
-
+restart

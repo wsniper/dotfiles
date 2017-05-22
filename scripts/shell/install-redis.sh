9e17629fc638f0af0fa6c5e10b7bbeb2184bd3e5
garden.yuen <weaming@users.noreply.github.com>
2017-05-22 14:18:03 +0800

    Update install-redis.sh

--- scripts/shell/install-redis.sh
+++ scripts/shell/install-redis.sh
@@ -3,4 +3,7 @@
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz && rm -rf redis-stable.tar.gz
 cd redis-stable
-make && make install && cd .. && rm -rf redis-stable
+make && sudo make install && \
+        sudo cp redis.conf /etc/redis.conf && \
+        sudo cp redis.conf /etc/redis.conf.example && \
+        cd .. && rm -rf redis-stable
